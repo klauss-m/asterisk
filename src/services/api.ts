e6014c8cf94d59c63ccad16f227f9a87ee25305d
Klauss Marques <klauss.marques@gmail.com>
2021-11-21 01:18:39 -0300

    chore: new environment variable

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://asteriskapi.herokuapp.com/',
+  baseURL: process.env.REACT_APP_BACKEND,
   headers: { 'Access-Control-Allow-Origin': '*' },
 })
 
